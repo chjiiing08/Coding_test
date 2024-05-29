13a450c7212eef9ff464f98fdfe9e4d0229a762b
CHOHYUNJIN1234 <165138839+CHOHYUNJIN1234@users.noreply.github.com>
2024-05-29 18:23:57 +0900

    [level 0] Title: 문자열 돌리기, Time: 131.56 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch;
+        String a = sc.next();
+        for(int i=0; i<a.length(); i++){
+            ch=a.charAt(i);
+            System.out.println(ch);
+        }
+            
+        
+    }
+}
