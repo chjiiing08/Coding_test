d8172c65e37a1d94f5a199a1e2b732b5b4071139
조현진 <165138839+chjiiing08@users.noreply.github.com>
2025-02-08 17:21:10 +0900

    [level 0] Title: 이어 붙인 수, Time: 12.85 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String h="";
+        String j="";
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]%2==0)
+                j+=num_list[i];
+            else h+=num_list[i];
+        }
+        answer=Integer.parseInt(j)+Integer.parseInt(h);
+        return answer;
+    }
+}
