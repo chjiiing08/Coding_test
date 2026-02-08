af3b774d8720c6751d771b4c9fe4c1c68203322e
조현진 <165138839+chjiiing08@users.noreply.github.com>
2026-02-08 19:20:22 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 32.26 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,10 +1,4 @@
 function solution(n) {
-    var answer = 0;
-    for(let i = 1; i<n; i++){
-        if(n%i==1) {
-            answer=i;
-            break;
-        }
-    } 
-    return answer;
+    for(let i = 1; i<n; i++) if(n%i==1) return i;
+    
 }
